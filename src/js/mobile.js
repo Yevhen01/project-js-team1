237c0241cecdd3ad6a36181a4990feada7e72816
Andrii2024 <krovlya2005@gmail.com>
2024-02-04 15:46:35 +0200

    update04.02.24

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,10 +1,11 @@
 // =====================================
 // оновлений
+
 (() => {
   const refs = {
-    homeLink: document.querySelector('.page-nav-link[href="../index.html"]'),
+    homeLink: document.querySelector('.page-nav-link[href="./index.html"]'),
     favoritesLink: document.querySelector(
-      '.page-nav-link[href="../favorites.html"]'
+      '.page-nav-link[href="./favorites.html"]'
     ),
     pageHeader: document.querySelector('#page-header'),
     toggleMenuBtn: document.querySelector('#toggle-mobile-menu-btn'),
@@ -33,6 +34,11 @@
 
     event.target.style.backgroundColor = '#f6f6f6';
     event.target.classList.add('current');
+    if (window.innerWidth >= 768) {
+      const href = event.target.getAttribute('href');
+
+      window.location.href = href;
+    }
   }
 
   function closeMenu() {
@@ -51,3 +57,68 @@
     refs.logo.classList.toggle('is-hidden');
   }
 })();
+
+// ==================================================
+
+// (() => {
+//   const refs = {
+//     homeLink: document.querySelector('.page-nav-link[href="./index.html"]'),
+//     favoritesLink: document.querySelector(
+//       '.page-nav-link[href="./favorites.html"]'
+//     ),
+//     pageHeader: document.querySelector('#page-header'),
+//     toggleMenuBtn: document.querySelector('#toggle-mobile-menu-btn'),
+//     menu: document.querySelector('#mobile-menu-container'),
+//     navLinks: document.querySelectorAll('.page-nav-link'),
+//     openMenuSvg: document.querySelector('#icon-burger'),
+//     closeMenuSvg: document.querySelector('#icon-close'),
+//     logo: document.querySelector('.logo'),
+//   };
+
+//   refs.homeLink.addEventListener('click', handleLinkClick);
+//   refs.favoritesLink.addEventListener('click', handleLinkClick);
+//   refs.toggleMenuBtn.addEventListener('click', toggleMenu);
+//   refs.navLinks.forEach(navLink => {
+//     navLink.addEventListener('click', closeMenu);
+//   });
+
+//   function handleLinkClick(event) {
+//     event.preventDefault();
+
+//     const allLinks = document.querySelectorAll('.page-nav-link');
+
+//     allLinks.forEach(link => {
+//       link.style.backgroundColor = 'transparent';
+//       link.classList.remove('current');
+//     });
+
+//     event.target.style.backgroundColor = '#f6f6f6';
+//     event.target.classList.add('current');
+
+//     if (window.innerWidth >= 768) {
+//       const href = event.target.getAttribute('href');
+
+//       window.location.href = href;
+//     }
+//   }
+
+//   function closeMenu() {
+//     refs.menu.classList.add('is-hidden');
+//     refs.pageHeader.classList.remove('fixed');
+//     refs.closeMenuSvg.classList.add('is-hidden');
+//     refs.openMenuSvg.classList.remove('is-hidden');
+//     refs.logo.classList.remove('is-hidden');
+//   }
+
+//   function toggleMenu() {
+//     refs.pageHeader.classList.toggle('fixed');
+//     refs.menu.classList.toggle('is-hidden');
+//     refs.closeMenuSvg.classList.toggle('is-hidden');
+//     refs.openMenuSvg.classList.toggle('is-hidden');
+//     refs.logo.classList.toggle('is-hidden');
+//   }
+// })();
+
+// ====================================================
+
+//
