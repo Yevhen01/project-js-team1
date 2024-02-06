6f4deaae12da63059fcc040dc943c846effdcb79
Kateryna02 <150830031+Kateryna02@users.noreply.github.com>
2024-02-06 18:32:13 +0100

    updet ssc

--- src/js/footer.js
+++ src/js/footer.js
@@ -8,27 +8,9 @@ const emailInput = document.getElementById('email');
 subscriptionForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
-
-<<<<<<< Updated upstream
     const emailValue = emailInput.value.trim();
-=======
-  if (!isValidEmail(emailValue)) {
-    iziToast.error({
-      title: 'error',
-      message: 'Sorry, not valid email. Please try again!',
-      position: 'bottomRight',
-      messageColor: '#f6f6f6',
-      backgroundColor: '#7e847f',
-        maxWidth: 300,
-    });
-    emailInput.value = '';
-    return;
-  }
->>>>>>> Stashed changes
-
 
     if (!isValidEmail(emailValue)) {
-
         iziToast.error({
             title: 'error',
             message: 'Sorry, not valid email. Please try again!',
@@ -44,30 +26,24 @@ subscriptionForm.addEventListener('submit', function (event) {
     sendSubscriptionRequest(emailValue);
 });
 
-
 function isValidEmail(email) {
     const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
     return emailRegex.test(email);
 }
 
 function sendSubscriptionRequest(email) {
-
     const backendURL = 'https://energyflow.b.goit.study/api/subscription';
     const data = {
         email: email,
-
     };
-
 
     const requestOptions = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-
         },
         body: JSON.stringify(data),
     };
-
 
     fetch(backendURL, requestOptions)
         .then(response => {
