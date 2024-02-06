8f82df70a59bc717390e0774dd9a5423daa65fcb
Yevhen02 <153821695+Yevhen02@users.noreply.github.com>
2024-02-06 15:17:04 +0200

    update footer js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './js/navigation';
 import './js/mobile';
 import './js/qoute';
 import './js/exercises';
-import './js/modal';
+// import './js/modal';
 import './js/footer';
-import './js/favoritelist';
+// import './js/favoritelist';
