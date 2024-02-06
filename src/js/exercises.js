0eac34c8d657610f9ee9ff01c2cac688ebc74898
Yevhen02 <153821695+Yevhen02@users.noreply.github.com>
2024-02-06 18:30:01 +0200

    remove comments in exercises js file

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -1,5 +1,5 @@
 import { getFilter, getExercises, getExercisesById } from './fitnesapi';
-// import { openExerciseModal } from './modal';
+import { openExerciseModal } from './modal';
 
 const refs = {
   musclesBtn: document.querySelector('.muscles-btn'),
@@ -358,7 +358,7 @@ function onExercisesClick(event) {
 async function onArrowClick(exerciseId) {
   try {
     const exerciseDetails = await getExercisesById(exerciseId);
-    // openExerciseModal(exerciseDetails);
+    openExerciseModal(exerciseDetails);
   } catch (error) {
     console.log(error);
   }
