6beccbec58e4435c9473692e07b11e69ff6a6ab8
Andrii2024 <krovlya2005@gmail.com>
2024-02-08 13:05:23 +0200

    update nav.js

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,5 +1,21 @@
 // ==================================================
 
+// document.addEventListener('DOMContentLoaded', () => {
+//   const path = window.location.pathname;
+//   const page = path === '/' ? 'index.html' : path.split('/').pop();
+//   const links = document.querySelectorAll('.page-nav-list a');
+
+//   links.forEach(link => {
+//     const href = link.getAttribute('href');
+//     if (href.includes(page)) {
+//       link.classList.add('current');
+//     }
+//   });
+// });
+
+// ====================================================
+// ====================================================
+
 document.addEventListener('DOMContentLoaded', () => {
   const path = window.location.pathname;
   const page = path === '/' ? 'index.html' : path.split('/').pop();
@@ -7,11 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   links.forEach(link => {
     const href = link.getAttribute('href');
+
     if (href.includes(page)) {
       link.classList.add('current');
+    } else {
+      link.classList.remove('current');
     }
   });
 });
-
-// ====================================================
-// ====================================================
